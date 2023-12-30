5234cea8fbf79ac25445e3b23b0e1424fa2ab0f3
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-12-30 20:29:44 +0100

    added first integration test

---
+++ MySpot.Tests.Integration/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace MySpot.Tests.Integration.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly MySpotTestApp _app;
+
+        public HomeControllerTests()
+        {
+            _app = new MySpotTestApp();
+        }
+
+        [Fact]
+        public async Task GetBaseEndpoint_ForValidRequest_ShouldReturnOkStatusCode()
+        {
+            var response = await _app.Client.GetAsync("/");
+            response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+            var content = await response.Content.ReadAsStringAsync();
+            content.ShouldBe("MySpot API [test]");
+        }
+    }
+}
